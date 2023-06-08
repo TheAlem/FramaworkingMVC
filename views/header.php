b3cd1708145a3b81d5524f6de1c6830bc653779c
Jose Alem <josealem03@gmail.com>
2023-06-08 19:12:35 -0400

    A ver

--- views/header.php
+++ views/header.php
@@ -1,7 +1,7 @@
 <div id="header">
     <ul>
         <li><a href="main">INICIO</a></li>
-        <li><a href="categoria">CATEGORIA</a></li>
+        <li><a href="producto">PRODUCTO</a></li>
         <li><a href="errores">ERRORES</a></li>
     </ul>
 </div>
--- views/producto/index.php
+++ views/producto/index.php
@@ -5,10 +5,11 @@
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <title>Page Title</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <link rel="stylesheet" href="public/default.css">
     <script src='main.js'></script>
 </head>
 <body>
+    <?php require "views/footer.php"; ?>
     <p>Estas en la vista de producto</p>
 </body>
 </html>
